e309bb5a1e6fbdfde9a65d6e829a74acf3eb0e32
Reginaldo <porffiriocosta@gmail.com>
2022-05-13 20:08:01 -0300

    Project created

---
+++ front-web/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import Filter from './components/filter';
+import Header from './components/header';
+
+import './App.css';
+import SalesSummary from './components/sales-summary';
+import { Store } from './types';
+
+function App() {
+
+   const [focusStore, setFocusStore] = useState<Store>();
+
+   const onFilterChange = (store: Store) => {
+      setFocusStore(store);
+   }
+
+   return (
+      <>
+         <Header />
+         <div className="app-container">
+            <Filter onFilterChange={onFilterChange}/>
+            <div className="sales-overview-container">
+               <SalesSummary store={focusStore}/>
+            </div>
+         </div>
+      </>
+   );
+}
+
+export default App;
